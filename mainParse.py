a7ae04ab601b20da94181f848fb1b8959fdaf85f
Iustinianus <suigintounotsuikaima@gmail.com>
2017-07-28 09:19:41 +0000

    release ver

--- mainParse.py
+++ mainParse.py
@@ -10,22 +10,31 @@ def getHTMLText(url):
 	body=BeautifulSoup(data, 'lxml')
 	return body
 
-def getAddressFromPage(body, shopOnPage=5):
+def getInfFromTable(body,shopOnPage=5):
 	addr=[]
 	tableTag=body.find('div', {'id':'DispListArticle'}).find('table', {'class':'cz_table01 cz_clear'}).find('tbody').findChildren('tr')
 	for i in range(shopOnPage):
 		row=tableTag[i].findChildren('td')
-		addr.append(row[1].text)
+		ul=tableTag[i].find('ul', {'class': 'cz_terms_icon'})
+		img=ul.findChildren('img')
+		alt=[]
+		for j in img:
+			alt.append(j.get('alt', ''))
+		shopType=row[0].find('img').get('alt', '')
+		addr.append('\n'+shopType+row[0].text+'\n'+row[1].text)
+		addr=addr+alt
+#	for a in addr:
+#		print(a)
 	return addr
 
-body=getHTMLText('http://as.chizumaru.com/famima/articleList?account=famima&accmd=0&c2=1&c1=1')
+body=getHTMLText('http://as.chizumaru.com/famima/articleList?account=famima&accmd=0&c2=1&c1=1,2,3')
 selectTag=body.find('select', {'name':'SelectListAdr1'})
 prefecture_list=selectTag.findChildren('option')
 
 for prefecture in prefecture_list:
 	if (prefecture["value"]!="00"):#select all except first string
 		prIndex=prefecture["value"]
-		body=getHTMLText('http://as.chizumaru.com/famima/articleList?c1=1&template=Ctrl/DispListArticle_g12&pageLimit=10000&pageSize=5&account=famima&accmd=0&bpref=%s&c2=1'%prIndex)
+		body=getHTMLText('http://as.chizumaru.com/famima/articleList?c1=1,2,3&template=Ctrl/DispListArticle_g12&pageLimit=10000&pageSize=5&account=famima&accmd=0&bpref=%s&c2=1'%prIndex)
 		#get page count
 		shopCount=int(body.find('div', {'id':'DispListArticle'}).find('div', {'class':'cz_counter'}).find('strong').text)
 		if (shopCount%5==0):
@@ -33,15 +42,15 @@ for prefecture in prefecture_list:
 		else:
 			pageCount=int(shopCount/5)+1
 		addresses=[]
-		addresses=addresses+getAddressFromPage(body)
+		addresses=addresses+getInfFromTable(body)
 		#cose first page has alreary done start from second
 		i=2
 		while i<=pageCount:
-			body=getHTMLText('http://as.chizumaru.com/famima/articleList?c1=1&template=Ctrl/DispListArticle_g12&pageLimit=10000&pageSize=5&account=famima&accmd=0&bpref=%s&c2=1&searchType=True&pg=%d'%(prIndex, i))
+			body=getHTMLText('http://as.chizumaru.com/famima/articleList?c1=1,2,3&template=Ctrl/DispListArticle_g12&pageLimit=10000&pageSize=5&account=famima&accmd=0&bpref=%s&c2=1&searchType=True&pg=%d'%(prIndex, i))
 			if(i<pageCount):
-				addresses=addresses+getAddressFromPage(body)
+				addresses=addresses+getInfFromTable(body)
 			else:
-				addresses=addresses+getAddressFromPage(body,shopCount%5)
+				addresses=addresses+getInfFromTable(body, shopCount%5)
 			i=i+1
 
 		file=open('txt_files/'+prefecture.text+'.txt','w')
